a3e83cd19748a6f40718bf76f2ea5378629529ae
Roman <arrumanov@mail.ru>
2020-06-23 09:12:48 +0300

    new project

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Blazor_static_site
+namespace BlazorGitHubPages
 {
     public class Program
     {
